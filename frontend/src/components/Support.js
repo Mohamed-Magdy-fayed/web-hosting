4bbc36e2353f3ffb33995c2f08f2140a5e28a0a7
Mohamed-Magdy-fayed <mohamed.ahmed.sc2@gmail.com>
2023-02-07 17:19:18 +0200

    support page bug fix

--- frontend/src/components/Support.js
+++ frontend/src/components/Support.js
@@ -1,4 +1,4 @@
-import { Button, Typography } from '@material-tailwind/react'
+import { Button, Input, Typography } from '@material-tailwind/react'
 import React from 'react'
 import HomeSection from './shared/HomeSection'
 
@@ -70,6 +70,16 @@ const Support = () => {
             <Typography className='text-left text-lg md:text-xl lg:text-2xl' variant='paragraph'>Find answers to technical questions like transferring a domain, domain management, and troubleshooting.</Typography>
             <Button>Go to hep center</Button>
           </div>
+        </div>
+      </div>
+      <div className='bg-gray-100 p-8 flex flex-col w-full gap-4 items-start lg:flex-row lg:items-center lg:justify-between'>
+        <div className=' max-w-md'>
+          <Typography className='text-left w-full mb-4' variant='lead'>Stay up to date with Google Domains</Typography>
+          <Typography className='text-left w-full text-gray-600' variant='small'>Sign up for Google Domains newsletters with product updates, special offers, and insights.</Typography>
+        </div>
+        <div className='flex gap-2 max-h-min w-full max-w-md'>
+          <Input variant="static" placeholder="Enter your email address" />
+          <Button className='bg-primary h-full whitespace-nowrap'>Sign Up</Button>
         </div>
       </div>
     </div>
